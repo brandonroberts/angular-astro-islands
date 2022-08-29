10b84b9b98ff1c729dbc86df67d15a89f9df5119
Brandon Roberts <robertsbt@gmail.com>
2022-08-29 10:19:35 -0500

    refactor: use NgIf instead of CommonModule

--- src/components/hello.component.ts
+++ src/components/hello.component.ts
@@ -1,10 +1,10 @@
-import { CommonModule } from '@angular/common';
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-hello',
   standalone: true,
-  imports: [CommonModule],
+  imports: [NgIf],
   template: `
     <p>Hello from Angular!!</p>
 
